80c6714f008dade4803973893ad7434d27f56d33
RayanXweb <rohmanjakarta15@gmail.com>
2026-06-07 13:30:00 +0700

    Create validators.js

---
+++ src/lib/validators.js
@@ -0,0 +1,66 @@
+export const isValidEmail = (email) => {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+};
+
+export const isValidPhone = (phone) => {
+  const re = /^[\+]?[(]?[0-9]{1,4}[)]?[-\s\.]?[(]?[0-9]{1,4}[)]?[-\s\.]?[0-9]{1,5}[-\s\.]?[0-9]{1,5}$/;
+  return re.test(phone);
+};
+
+export const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const isValidPassword = (password) => {
+  // At least 8 characters, 1 uppercase, 1 lowercase, 1 number
+  const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  return re.test(password);
+};
+
+export const isValidPin = (pin) => {
+  const re = /^\d{4,6}$/;
+  return re.test(pin);
+};
+
+export const isValidDeviceId = (deviceId) => {
+  const re = /^[a-zA-Z0-9\-_]{8,64}$/;
+  return re.test(deviceId);
+};
+
+export const isValidIP = (ip) => {
+  const ipv4Re = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  const ipv6Re = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/;
+  return ipv4Re.test(ip) || ipv6Re.test(ip);
+};
+
+export const validateInput = (value, rules) => {
+  const errors = [];
+  
+  if (rules.required && !value) {
+    errors.push('This field is required');
+  }
+  
+  if (rules.minLength && value?.length < rules.minLength) {
+    errors.push(`Minimum length is ${rules.minLength} characters`);
+  }
+  
+  if (rules.maxLength && value?.length > rules.maxLength) {
+    errors.push(`Maximum length is ${rules.maxLength} characters`);
+  }
+  
+  if (rules.pattern && !rules.pattern.test(value)) {
+    errors.push(rules.patternMessage || 'Invalid format');
+  }
+  
+  if (rules.custom && !rules.custom(value)) {
+    errors.push(rules.customMessage || 'Invalid value');
+  }
+  
+  return errors;
+};
